eb25c5b1037a21953445e11c16856c274e937d4a
ethan-chu-shlawg <143014840+ethan-chu-shlawg@users.noreply.github.com>
2024-03-27 22:47:27 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,13 +7,23 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    
+	      digitList = new ArrayList<Integer>();
+	      if(num == 0)
+	        digitList.add(0);
+	      while(num > 0){
+	        int yeah = num%10;
+	        digitList.add(0,yeah);
+	        num = num/10;
+	      }
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
-
+	    for(int i = 1; i < digitList.size(); i++){
+	      if(digitList.get(i-1) >= digitList.get(i))
+	        return false;
+	    }
+	    return true;
 	}
 	
 	public String toString()
